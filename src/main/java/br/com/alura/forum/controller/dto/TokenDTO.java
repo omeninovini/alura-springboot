0bad36aba5553c03fc30756a6c116c96eead925f
Vinicius <vini.martinslol@gmail.com>
2020-09-10 16:32:22 -0300

    Projeto realizado no curso da alura

---
+++ src/main/java/br/com/alura/forum/controller/dto/TokenDTO.java
@@ -0,0 +1,19 @@
+package br.com.alura.forum.controller.dto;
+
+public class TokenDTO {
+    private String token;
+    private String bearer;
+
+    public TokenDTO(String token, String bearer) {
+        this.token = token;
+        this.bearer = bearer;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public String getBearer() {
+        return bearer;
+    }
+}
---
+++ src/main/java/br/com/alura/forum/controller/dto/TopicoDTO.java
@@ -0,0 +1,42 @@
+package br.com.alura.forum.controller.dto;
+import br.com.alura.forum.models.Topico;
+import org.springframework.data.domain.Page;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class TopicoDTO {
+
+    private Long id;
+    private String titulo;
+    private String mensagem;
+    private LocalDateTime dataCriacao;
+
+    public TopicoDTO(Topico topico) {
+        this.id = topico.getId();
+        this.titulo = topico.getTitulo();
+        this.mensagem = topico.getMensagem();
+        this.dataCriacao = topico.getDataCriacao();
+    }
+
+    public static Page<TopicoDTO> converter(Page<Topico> topicos) {
+        return topicos.map(TopicoDTO::new);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public LocalDateTime getDataCriacao() {
+        return dataCriacao;
+    }
+}
